d7dcbea64af9f117b5fad9c55da2776d8d130ec5
KunalSanghvii <kunalsanghavi08@gmail.com>
2025-04-12 21:12:08 -0400

    AI setup

---
+++ main.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+#from tools import search_tool, wiki_tool, save_tool
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    source: list[str]
+    tools_used: list[str]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use necessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[],verbose =True)
+raw_response = agent_executor.invoke({"query": "how many rivers are there in Cuenca Ecuador ?"})
+print(raw_response)
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
+
